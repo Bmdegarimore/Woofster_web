fa2a29282dd3da2818668b4cb43c57624d89e3ab
Corey Johnson <corey@coreys-mbp.tech.div>
2016-04-20 07:56:22 -0700

    Added sign in, commented out site.js
    Commented out because it's not working properly, breaking functionality
    in the sign in because it’s referencing something that doesn’t exist.

---
+++ hybridauth/config.php
@@ -0,0 +1,23 @@
+<?php
+/*!
+* HybridAuth
+* http://hybridauth.sourceforge.net | http://github.com/hybridauth/hybridauth
+* (c) 2009-2012, HybridAuth authors | http://hybridauth.sourceforge.net/licenses.html
+*/
+
+// ----------------------------------------------------------------------------------------
+//	HybridAuth Config file: http://hybridauth.sourceforge.net/userguide/Configuration.html
+// ----------------------------------------------------------------------------------------
+$config =array(
+		"base_url" => "http://woofster.greenrivertech.net/hybridauth/hybridauth/index.php", 
+		"providers" => array ( 
+
+			"Google" => array ( 
+				"enabled" => true,
+				"keys"    => array ( "id" => "77308977075-d6krcuc0khaa7ofqikabr75vap2vemp6.apps.googleusercontent.com", "secret" => "zsH95AemdKkv-Bfsgk0HgXGM" ), 
+			)
+		),
+		// if you want to enable logging, set 'debug_mode' to true  then provide a writable file by the web server on "debug_file"
+		"debug_mode" => false,
+		"debug_file" => "",
+	);
