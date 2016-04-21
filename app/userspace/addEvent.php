746b069cc6b5212afcf16f78f83191d794193eeb
Casey Morris <casemorris@hotmail.com>
2016-04-21 16:32:07 -0700

    updated name per inputs in event add/delete/edit

--- app/userspace/addEvent.php
+++ app/userspace/addEvent.php
@@ -4,34 +4,36 @@
     <form  class="form-horizontal" method="post" action="?select=events">
        <div class="col-sm-6">
             <label for="eventTitle" class="control-label">Event Title</label>
-            <input type="text" class="form-control" id="eventTitle">
+            <input type="text" class="form-control" name="eventTitle" id="eventTitle">
         </div>
         <div class="col-sm-6">
             
             <label for="eventDate" class="control-label">Event Date</label>
-            <input type="text" class="form-control" id="eventDate">
+            <input type="text" class="form-control" name="eventDate" id="eventDate">
         </div>
         <div class="col-sm-6">
        
             <label for="isRepeated" class="control-label">Repeated</label>
-            <input type="text" class="form-control" id="isRepeated">
+            <input type="text" class="form-control" name="isRepeated" id="isRepeated">
         </div>
         <div class="col-sm-6">
             <label for="repeatFreq" class="control-label">How Frequent</label>
-            <input type="text" class="form-control" id="repeatFreq">
+            <input type="text" class="form-control" name="repeatFreq" id="repeatFreq">
         </div>
        
         <div class="col-sm-12">
             <label for="notes" class="control-label">Notes</label>
-            <textarea class="form-control" rows="4" id="notes"></textarea>
+            <textarea class="form-control" rows="4" name="notes" id="notes"></textarea>
         </div>
         
         <div class="row">
             <br>
             <div class="col-sm-4">
+            <br>
             <button type="submit" class="btn btn-primary btn-lg" name="cancel">Cancel</button>
             </div>
             <div class="col-sm-4 col-sm-offset-4">
+            <br>
             <button type="submit" class="btn btn-primary btn-lg" name="update" value="delete">Add</button>
             </div>
         </div>
--- app/userspace/editEvent.php
+++ app/userspace/editEvent.php
@@ -5,33 +5,35 @@
     
         <div class="col-sm-6">
             <label for="eventTitle" class="control-label">Event Title</label>
-            <input type="text" class="form-control" id="eventTitle" value="<?php if(isset($_GET['row'])){echo($events[$_GET['row']][title]);}; ?>">
+            <input type="text" class="form-control" name="eventTitle" id="eventTitle" value="<?php if(isset($_GET['row'])){echo($events[$_GET['row']][title]);}; ?>">
         </div>
         <div class="col-sm-6">
             
             <label for="eventDate" class="control-label">Event Date</label>
-            <input type="text" class="form-control" id="eventDate" value="<?php if(isset($_GET['id'])){echo($events[$_GET['row']][eventDate]);}; ?>">
+            <input type="text" class="form-control" name="eventDate" id="eventDate" value="<?php if(isset($_GET['id'])){echo($events[$_GET['row']][eventDate]);}; ?>">
         </div>
         <div class="col-sm-6">
        
             <label for="isRepeated" class="control-label">Repeated</label>
-            <input type="text" class="form-control" id="isRepeated" value="<?php if(isset($_GET['id'])){echo($events[$_GET['row']][repeated]);}; ?>">
+            <input type="text" class="form-control" name="isRepeated" id="isRepeated" value="<?php if(isset($_GET['id'])){echo($events[$_GET['row']][repeated]);}; ?>">
         </div>
         <div class="col-sm-6">
             <label for="repeatFreq" class="control-label">How Frequent</label>
-            <input type="text" class="form-control" id="repeatFreq" value="<?php if(isset($_GET['id'])){echo($events[$_GET['row']][repeatFrequency]);}; ?>">
+            <input type="text" class="form-control" name="repeatFreq" id="repeatFreq" value="<?php if(isset($_GET['id'])){echo($events[$_GET['row']][repeatFrequency]);}; ?>">
         </div>
        
         <div class="col-sm-12">
             <label for="notes" class="control-label">Notes</label>
-            <textarea class="form-control" rows="4" id="notes"><?php if(isset($_GET['id'])){echo($events[$_GET['row']][notes]);}; ?></textarea>
+            <textarea class="form-control" rows="4" name="notes" id="notes"><?php if(isset($_GET['id'])){echo($events[$_GET['row']][notes]);}; ?></textarea>
         </div>
         
         <div class="row">
             <div class="col-sm-4">
+            <br>
             <button type="submit" class="btn btn-primary btn-lg" name="cancel">Cancel</button>
             </div>
             <div class="col-sm-4 col-sm-offset-4">
+            <br>
             <button type="submit" class="btn btn-primary btn-lg" name="update" value="update">Update</button>
             </div>
         </div>
