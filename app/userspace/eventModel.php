8cda726b383a9c032f5d1b84cc9be22d8944bcb2
Corey Johnson <cor.johnson@me.com>
2016-04-29 15:34:43 -0700

    reformatted code
    Code needed to be restructured, also changed delete event so it moves
    the event to the deleted events table

--- app/userspace/eventModel.php
+++ app/userspace/eventModel.php
@@ -4,111 +4,128 @@ session_start();
 $uidstr = $_SESSION['user_uniqueId'];
 
 
-    class EventModel{
-        
-        // Place to store the database connection
-        protected static $connection;
-        
-        public function connect(){
-            try {
-                // Retrieves data needed to connect to data base via config.ini
-                $config = parse_ini_file('../../../config/config.ini');
-                
-                // Attempts to connect to database
-                self::$connection = new PDO($config['dbname'], $config['username'], $config['password']);
-              
-                } catch (PDOException $e) {
-                    // Displays error message need to change when in production to clean error message
-                    print "Error!: " . $e->getMessage() . "<br/>";
-                    die();
-                    return false;
-                }
-                
-		return self::$connection;
-        
-        }
-        
-        public function disconnect(){
-            self::$connection = null;
-        }
-        
-        public function selectEvents($loginID){
-           try{
-                // Creates a prepared select statement
-                $statement = self::$connection->prepare("select * from events where unique_loginID = :loginID");
-                // $statement = self::$connection->prepare("SELECT * FROM Dogs INNER JOIN Events ON Dogs.dogID = Events.dogID WHERE Dogs.unique_loginID =d41d8cd98f00b204e9800998ecf8427e order by dogs.name");
-                // References namespace of dog to query
-                $statement->bindParam(':loginID', $loginID, PDO::PARAM_STR);
-                $statement->execute();
+class EventModel {
 
-                // Returns selected rows
-                $row = $statement->fetchAll();
+  // Place to store the database connection
+  protected static $connection;
 
-            } catch(PDOException  $e ){
-                print "Error!: " . $e->getMessage() . "<br/>";
-                return false;
-            }
+  public function connect(){
+    try {
+      // Retrieves data needed to connect to data base via config.ini
+      $config = parse_ini_file('../../../config/config.ini');
 
-            return $row;
-        }
-        
-        // Adds a new event to event table
-        public function insertEvent($title,$eventDate,$notes,$unique_loginID){
-            try{
-	       //Insert new events
-	       $statement = self::$connection->prepare("INSERT INTO events (title,eventDate,notes,unique_loginID)VALUES(:title,:eventDate,:notes,:unique_loginID)");
-	       $statement->bindParam(':title', $title, PDO::PARAM_STR);
-	       $statement->bindParam(':eventDate', $eventDate, PDO::PARAM_STR);
-	       $statement->bindParam(':notes',$notes, PDO::PARAM_STR);
-	       $statement->bindParam(':unique_loginID', $unique_loginID, PDO::PARAM_STR);		
-	       
-	       $statement->execute();
-	    } catch(PDOException  $e )
-	  {
-               print "Error!: " . $e->getMessage() . "<br/>";
-               return false;
-          }	  
-        }
-        
+      // Attempts to connect to database
+      self::$connection = new PDO($config['dbname'], $config['username'], $config['password']);
 
-        public function deleteEvent($eventID,$loginID)
-	{
-	  try{
-	       // Deletes an event based on eventID and loginID
-               $statement = self::$connection->prepare("DELETE FROM `events` WHERE eventID = :eventID and unique_loginID = :loginID");
-               $statement->bindParam(':eventID', $eventID, PDO::PARAM_INT);
-	       $statement->bindParam(':loginID', $loginID, PDO::PARAM_STR);
-               $statement->execute();
-
-	  } catch(PDOException  $e )
-	  {
-               print "Error!: " . $e->getMessage() . "<br/>";
-               return false;
-          }	  
-	}
-	
-	public function updateEvent($title,$eventDate,$notes,$eventID,$unique_loginID)
-	{
-	  try{
-	       // Update an event based on eventID and loginID
-	       //$testTitle,$testDate,$testFreq,$testRepeatFreq,$testNotes,$testEventID,$testUnique
-              //Insert new events
-	       $statement = self::$connection->prepare("UPDATE events SET title = :title, eventDate = :eventDate, notes= :notes WHERE eventID = :eventID and unique_loginID = :unique_loginID");
-	       $statement->bindParam(':title', $title, PDO::PARAM_STR);
-	       $statement->bindParam(':eventDate', $eventDate, PDO::PARAM_STR);
-	       $statement->bindParam(':notes',$notes, PDO::PARAM_STR);
-	       $statement->bindParam(':eventID',$eventID, PDO::PARAM_STR);
-	       $statement->bindParam(':unique_loginID', $unique_loginID, PDO::PARAM_STR);	
-               $statement->execute();
-
-	  } catch(PDOException  $e )
-	  {
-               print "Error!: " . $e->getMessage() . "<br/>";
-               return false;
-          }	  
-	}
-        
-        
+    } catch (PDOException $e) {
+      // Displays error message need to change when in production to clean error message
+      print "Error!: " . $e->getMessage() . "<br/>";
+      die();
+      return false;
     }
+
+    return self::$connection;
+  }
+
+  public function disconnect(){
+    self::$connection = null;
+  }
+
+  public function selectEvents($loginID){
+    try{
+      // Creates a prepared select statement
+      $statement = self::$connection->prepare("select * from events where unique_loginID = :loginID");
+      // $statement = self::$connection->prepare("SELECT * FROM Dogs INNER JOIN Events ON Dogs.dogID = Events.dogID WHERE Dogs.unique_loginID =d41d8cd98f00b204e9800998ecf8427e order by dogs.name");
+      // References namespace of dog to query
+      $statement->bindParam(':loginID', $loginID, PDO::PARAM_STR);
+      $statement->execute();
+
+      // Returns selected rows
+      $row = $statement->fetchAll();
+
+    } catch(PDOException  $e ){
+      print "Error!: " . $e->getMessage() . "<br/>";
+      return false;
+    }
+
+    return $row;
+  }
+
+  // Adds a new event to event table
+  public function insertEvent($title,$eventDate,$notes,$unique_loginID){
+    try{
+      //Insert new events
+      $statement = self::$connection->prepare("INSERT INTO events (title,eventDate,notes,unique_loginID)VALUES(:title,:eventDate,:notes,:unique_loginID)");
+      $statement->bindParam(':title', $title, PDO::PARAM_STR);
+      $statement->bindParam(':eventDate', $eventDate, PDO::PARAM_STR);
+      $statement->bindParam(':notes',$notes, PDO::PARAM_STR);
+      $statement->bindParam(':unique_loginID', $unique_loginID, PDO::PARAM_STR);		
+
+      $statement->execute();
+    } catch(PDOException  $e ) {
+      print "Error!: " . $e->getMessage() . "<br/>";
+      return false;
+    }	  
+  }
+
+
+  public function deleteEvent($eventID,$loginID) {
+    try{
+      //Grab the event from events table
+      $statement = self::$connection->prepare("SELECT FROM 'events' WHERE eventID = :eventID and unique_loginID = :loginID");
+      $statement->bindParam(':eventID', $eventID, PDO::PARAM_INT);
+      $statement->bindParam(':loginID', $loginID, PDO::PARAM_STR);
+      $statement->execute();
+
+      $row = $statement->fetchAll();
+
+      //Get the event data
+      $eventTitle =$row['title'];
+      $eventDate =$row['eventDate'];
+      $notes =$row['notes'];
+      $eventID = $row['eventID'];
+      $userID = $row['unique_loginID'];
+
+      //insert the event into the deleted events table
+      $statement = self::$connection->prepare("INSERT INTO deletedEvents (title,eventDate,notes,unique_loginID,eventID)VALUES(:title,:eventDate,:notes,:unique_loginID,:eventID)");
+      $statement->bindParam(':title', $title, PDO::PARAM_STR);
+      $statement->bindParam(':eventDate', $eventDate, PDO::PARAM_STR);
+      $statement->bindParam(':notes',$notes, PDO::PARAM_STR);
+      $statement->bindParam(':unique_loginID', $userID, PDO::PARAM_STR);
+      $statement->bindParam(':eventID', $eventID, PDO::PARAM_INT);
+      $statement->execute();
+
+      // Deletes an event based on eventID and loginID
+      $statement = self::$connection->prepare("DELETE FROM `events` WHERE eventID = :eventID and unique_loginID = :loginID");
+      $statement->bindParam(':eventID', $eventID, PDO::PARAM_INT);
+      $statement->bindParam(':loginID', $loginID, PDO::PARAM_STR);
+      $statement->execute();
+    } catch(PDOException  $e ) {
+      print "Error!: " . $e->getMessage() . "<br/>";
+      return false;
+    }	  
+  }
+
+  public function updateEvent($title,$eventDate,$notes,$eventID,$unique_loginID) {
+    try{
+      // Update an event based on eventID and loginID
+      //$testTitle,$testDate,$testFreq,$testRepeatFreq,$testNotes,$testEventID,$testUnique
+      //Insert new events
+      $statement = self::$connection->prepare("UPDATE events SET title = :title, eventDate = :eventDate, notes= :notes WHERE eventID = :eventID and unique_loginID = :unique_loginID");
+      $statement->bindParam(':title', $title, PDO::PARAM_STR);
+      $statement->bindParam(':eventDate', $eventDate, PDO::PARAM_STR);
+      $statement->bindParam(':notes',$notes, PDO::PARAM_STR);
+      $statement->bindParam(':eventID',$eventID, PDO::PARAM_STR);
+      $statement->bindParam(':unique_loginID', $unique_loginID, PDO::PARAM_STR);	
+      $statement->execute();
+
+    } catch(PDOException  $e ) {
+      print "Error!: " . $e->getMessage() . "<br/>";
+      return false;
+    }	  
+  }
+
+
+}
 
 ?>
