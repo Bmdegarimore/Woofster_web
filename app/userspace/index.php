a7b499ebfc31f65f87bffe067f0a398db567d3b6
Casey Morris <casemorris@hotmail.com>
2016-05-15 22:51:44 -0700

    disabled admin panel oil working properly

---
+++ app/userspace/index.php
@@ -0,0 +1,43 @@
+<?php
+   //Starting session so I can grab stuff from $_SESSION
+  session_start();
+
+  if(!isset($_SESSION['user_uid'])){
+    header("Location: ../../");
+  }
+    require("header.html");
+?>
+<body class="nav-md">
+        <?php
+            require("nav.php");
+        ?>
+        <div class="right_col" role="main">
+            <div class="row">
+            <!-- Page content goes here -->
+            <?php
+              if(isset($_GET['controller'])){
+                $controller = strtoupper($_GET['controller']);
+                //$action = strtoupper($_GET['action']);
+                switch ($controller) {
+                    case EVENTS:
+                        include('controller/controllerEvents.php');
+                        break;
+                    /*case ADMIN:
+                        echo "I'm an admin";
+                        break;*/
+                    default:
+                       include('controller/controllerEvents.php');
+                }
+              }else{
+                include('controller/controllerEvents.php');
+              }
+            ?>
+            </div>
+            <?php
+                require("footer.html");
+            ?>
+        </div>
+    <?php
+      include('scripts.php');
+    ?>
+</body>
