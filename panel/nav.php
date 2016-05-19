1beb87f6d30b1d00a9a0df1114a495dfeaf4110b
Casey Morris <casemorris@hotmail.com>
2016-05-18 21:37:31 -0700

    Cleaned up dead scripts and images from nav

--- panel/nav.php
+++ panel/nav.php
@@ -13,23 +13,8 @@
 
           <!-- menu prile quick info -->
           <div class="profile">
-            <div class="profile_pic">
-              <?php 
-                //Get the profile picture
-                if(isset($_SESSION['user_photoURL'])) { 
-                  echo "<img src='" . $_SESSION['user_photoURL'] . 
-                        "' alt='User profile picture' class='img-circle profile_img'>"; 
-                }
-              ?>
-            </div>
             <div class="profile_info">
               <span>Welcome Admin, </span>
-              <!--<?php 
-                //Show the user's name if it's set
-                /*if(isset($_SESSION['user_name'])) { 
-                  echo "<h2>" . $_SESSION['user_name'] . "</h2>"; 
-                }*/
-              ?>-->
             </div>
           </div>
           <!-- /menu prile quick info -->
@@ -42,7 +27,7 @@
             <div class="menu_section">
               <h3>Features</h3>
               <ul class="nav side-menu">
-                <li><a href="?controller=admin&update=events "><i class="fa fa-bars" ></i>Admin Events View</a></li>
+                <li><a href="?controller=admin$update=events"><i class="fa fa-bars" ></i>Admin Events View</a></li>
                 <li><a href="?controller=admin&update=showUsers"><i class="fa fa-envelope" aria-hidden="true" ></i> User Emails</a></li>
                     <?php
                     echo $_POST['update'];
@@ -64,12 +49,6 @@
             <ul class="nav navbar-nav navbar-right">
               <li class="">
                 <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-                  <?php 
-                    //Get the profile picture & username
-                    if(isset($_SESSION['user_name'])) { 
-                      echo $_SESSION['user_name']; 
-                    }
-                  ?>
                   <span class=" fa fa-angle-down"></span>
                 </a>
                 <ul class="dropdown-menu dropdown-usermenu pull-right">
--- panel/scripts.php
+++ panel/scripts.php
@@ -1,6 +1,6 @@
 <!-- Custom JS -->
-        <script src="$_SERVER['DOCUMENT_ROOT'].'app/userspace/assets/js/nprogress.js'"></script>
-        <script src="$_SERVER['DOCUMENT_ROOT'].'app/userspace/assets/js/custom.js'"></script>
+        <!--<script src="$_SERVER['DOCUMENT_ROOT'].'app/userspace/assets/js/nprogress.js'"></script>
+        <script src="$_SERVER['DOCUMENT_ROOT'].'app/userspace/assets/js/custom.js'"></script>-->
               
         <!--<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>-->
         <script src="//cdn.datatables.net/1.10.5/js/jquery.dataTables.min.js"></script>
@@ -19,77 +19,7 @@
                 "scrollCollapse": false,
                 "paging":         false
                 } );
-              
-              // Coverts time back to YYYY-MM-DD HH:mm:ss format
-              $('#button').click(function(){
-                var convertTime = moment($('#datetimepicker12').val()).format('YYYY-MM-DD HH:mm:ss');
-                $('#datetimepicker12').val(convertTime);
-                
-              });
-                
-                //When the button is clicked, show the appropriate modal
-                $('#modalAdd').on('show.bs.modal', function (event) {
-                        // Button that triggered the modal
-                    var button = $(event.relatedTarget); 
-                    
-                    // Extract info from data-* attributes
-                    var title = button.data('title'); 
-                    var changedValue = button.data('value');
-                    var buttonName = button.data('button');
-                    
-                    // Grabs row to edit or delete
-                    var rowSelected = button.data('row');
-                    
-                    var modal = $(this);
-                    // Updates the Title of Event to Add, Update, or Delete
-                    modal.find('.modal-title').text(title + " Event");
-                    modal.find('#button').text(buttonName + " Event");
-                    modal.find('#button').val(changedValue);
-                    
-                    //Make sure fields avail to edit basically resets to original state
-                    modal.find("button").removeClass('btn-danger');
-                    modal.find('#eventTitle').prop('disabled',false);
-                    modal.find('#notes').prop('disabled', false);
-
-                    // Sets the date time picker
-                        $('#datetimepicker12').datetimepicker({
-                          inline: true,
-                          sideBySide: true,
-                          useCurrent: true
-                          
-                        });
-                        $('#datetimepicker12').data("DateTimePicker").minDate(new Date());
-                    // Loads empty modal
-                    if (title == 'Add') {
-                        // Make sure add fields are blank
-                        rowSelected = null;
-                        modal.find("input[type=text], textarea").val("");
-                        
- 
-                    } else {
-                        // Populates data to see from event either delete or edit
-                        var eventID = button.data('event');
-                        modal.find('#eventID').val(eventID);
-                        modal.find('#eventTitle').val(jsonEvents[rowSelected].title);
-                        modal.find('#notes').val(jsonEvents[rowSelected].notes);
-                          
-                        var testTime = jsonEvents[rowSelected].eventDate;
-                        // Set date and time based on existing date set
-                        $('#datetimepicker12').datetimepicker({
-                          format: 'YYYY-MM-DD hh:mm:ss'
-                        });
-                        
-                        // Uses moment.js to format time
-                        $('#datetimepicker12').data("DateTimePicker").date(moment(testTime));
-                       
-                        if (title == 'Delete') {
-                          //Adds read only to fields
-                          modal.find('#button').addClass("btn-danger");
-                          modal.find('#eventTitle').prop('disabled',true);
-                          modal.find('#notes').prop('disabled', true);
-                        }
-                    }
-                });
+  
             } );
         </script>
 </body>
