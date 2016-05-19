bc53bf483e27577b3d69cdfc46a47d956f988696
Casey Morris <casemorris@hotmail.com>
2016-05-19 00:01:06 -0700

    Fixed corrupted file showAllEvents.php and fixed anchor refs to load correctly

--- app/userspace/view/showAllEvents.php
+++ app/userspace/view/showAllEvents.php
@@ -1,22 +1,21 @@
-<?php ?> 
-    <h1 class="text-center">User Events Page:</h1>
+<h1 class="text-center">Admin Event List:</h1>
     <br>
+    
     <table id="example" class="display" cellspacing="0" width="100%">
         <thead>
             <tr>
-                <th>User</th>
-                <th>Event Title</th>
                 <th>Event Date</th>
+                <th>Email Address</th>
+                <th>Event Title</th>
                 <th>Notes</th>
-               
             </tr>
         </thead>
  
         <tfoot>
             <tr>
-                <th>User</th>
-                <th>Event Title</th>
                 <th>Event Date</th>
+                <th>Email Address</th>
+                <th>Event Title</th>
                 <th>Notes</th>
             </tr>
         </tfoot>
@@ -24,14 +23,12 @@
         <tbody>
           <?php
             foreach($events as $row){
-                $user = $row['username'];
+                $emailAddress = $row['username'];
                 $eventTitle =$row['title'];
                 $miltaryDate=$row['eventDate'];
                 $eventDate = date('m/d/Y h:i a', strtotime($miltaryDate));
                 $notes =$row['notes'];
-                $eventID = $row['eventID'];
-
-                echo "<tr><td>$user</td><td>$eventTitle</td><td class='timeDate'>$eventDate</td><td>$notes</td>></tdtr>";
+                echo "<tr><td class='timeDate'>$eventDate</td><td>$emailAddress</td><td>$eventTitle</td><td>$notes</td></tdtr>";
             };
            ?>
         </tbody>
--- panel/nav.php
+++ panel/nav.php
@@ -7,7 +7,7 @@
       <div class="col-md-3 left_col">
         <div class="left_col scroll-view">
           <div class="navbar nav_title" style="border: 0;">
-            <a href="./" class="site_title"><i class="fa fa-paw"></i> <span>Woofster!</span></a>
+            <a href="/panel" class="site_title"><i class="fa fa-paw"></i> <span>Woofster!</span></a>
           </div>
           <div class="clearfix"></div>
 
