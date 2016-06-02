f3eb98ffbd204f3d0852a817d752261b41696674
Melissa Curry <macurry81@gmail.com>
2016-06-02 04:12:18 -0700

    excel download working now
    still need styling changes, table cells aren’t big enough for data.

--- app/userspace/download.php
+++ app/userspace/download.php
@@ -36,7 +36,7 @@ if (PHP_SAPI == 'cli')
 
 /** Include PHPExcel */
 //require_once dirname(__FILE__) . '../Classes/PHPExcel.php';
-require "assets/Classes/PHPExcel.php";
+require_once"assets/Classes/PHPExcel.php";
 //require ($_SERVER['DOCUMENT_ROOT'].'/app/userspace/assets/Classes/PHPExcel.php');
 require "dbcon.php";
 //require ($_SERVER['DOCUMENT_ROOT'].'/app/userspace/dbcon.php');
@@ -86,18 +86,17 @@ $objPHPExcel->setActiveSheetIndex(0);
 
 
 // Redirect output to a client’s web browser (Excel2007)
-header('Content-Type:application/vnd.google-apps.spreadsheet');
 header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
 header('Content-Disposition:attachment;filename="EmailList.xlsx"');
 header('Cache-Control:max-age=0');
 // If you're serving to IE 9, then the following may be needed
 header('Cache-Control:max-age=1');
 
-/*// If you're serving to IE over SSL, then the following may be needed
+// If you're serving to IE over SSL, then the following may be needed
 header ('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
 header ('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT'); // always modified
 header ('Cache-Control: cache, must-revalidate'); // HTTP/1.1
-header ('Pragma: public'); // HTTP/1.0*/
+header ('Pragma: public'); // HTTP/1.0
 
 $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
 $objWriter->save('php://output');
